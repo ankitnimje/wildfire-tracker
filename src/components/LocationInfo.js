b4d47fe91e54c559aaa8c7e3616fc23e7a00989c
Ankit Nimje Account <ankit.nimje93@gmail.com>
2020-11-20 23:09:21 -0800

    Final changes

--- src/components/LocationInfo.js
+++ src/components/LocationInfo.js
@@ -2,7 +2,7 @@
 const LocationInfo = ({ info }) => {
     return (
         <div className="location-info">
-            <h2>Event Location Info <span id="close">x</span></h2>
+            <h2>Event Location Info</h2>
             <ul>
                 <li>ID: <strong>{ info.id }</strong></li>
                 <li>Title: <strong>{ info.title }</strong></li>
